ac95b70969fff4ca118ea917e03ffb6708aa655d
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-04 12:51:09 +0200

    organized Main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,19 @@
 import MainButtons from "./MainButtons";
 
 const Main = () => {
+  const mainLogo = (
+    <img
+      className="logo"
+      src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+      alt="Google logo"
+    />
+  );
+  const searchBar = <input className="search-bar" type="search" />;
+
   return (
     <main>
-      <img
-        className="logo"
-        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-        alt="Google logo"
-      />
-      <input className="search-bar" type="search" />
+      {mainLogo}
+      {searchBar}
       <MainButtons />
     </main>
   );
